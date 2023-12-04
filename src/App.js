1b51866f9f16920a2d82b1201b4390e5eec78d85
OverInk <dostoevskiy201@mail.ru>
2023-12-04 05:20:54 +0300

    New

--- src/App.js
+++ src/App.js
@@ -4,14 +4,55 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h3>Корзина</h3>
-          <div className="cartItem d-flex">
-            <img src="/img/sneakers/2.svg" alt="sneaker " />
-            <div>
-              <p>Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 990 руб.</b>
+          <h3 className="mb-30">Корзина</h3>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              {/* <img
+              className="mr-20"
+              width={70}
+              height={70}
+              src="/img/sneakers/2.svg"
+              alt="sneaker "
+            /> */}
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/2.svg)' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 990 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/remove.svg" alt="remove" />
             </div>
-            <img src="/img/remove.svg" alt="remove" />
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/2.svg)' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 990 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/remove.svg" alt="remove" />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="arrow" />{' '}
+            </button>
           </div>
         </div>
       </div>
