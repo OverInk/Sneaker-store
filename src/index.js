69945a423be1c0b11f9324be2d0768ed49746d51
OverInk <dostoevskiy201@mail.ru>
2023-12-21 17:43:38 +0300

    New

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import 'macro-css';
@@ -8,8 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
